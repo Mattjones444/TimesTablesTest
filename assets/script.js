25db2d4c83320610a11c229bb9fcc62f8a089ed1
Mattjones444 <>
2023-11-16 17:09:25 +0000

    Added rules page, start button links and game page

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+$('.start-button').on('click', function () {
+    window.location.href = 'rules.html';
+});
+
+$('.start-button1').on('click', function () {
+    window.location.href = 'game.html';
+});
