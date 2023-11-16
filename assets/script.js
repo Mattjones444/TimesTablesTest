0b22e424fc493a2167fd07198deabe97d66803cc
Mattjones444 <>
2023-11-16 21:44:24 +0000

    Added game screen and JS to load new questions

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,5 @@
+/* Moving from Start pages*/
+
 $('.start-button').on('click', function () {
     window.location.href = 'rules.html';
 });
@@ -5,3 +7,16 @@ $('.start-button').on('click', function () {
 $('.start-button1').on('click', function () {
     window.location.href = 'game.html';
 });
+
+const num1 = 3;
+var num2 = Math.floor(Math.random() * (12 - 0 + 1) + 0);
+$('.num1').html(num1);
+$('.num2').html(num2);
+const correctAnswer = num1 * num2;
+console.log(correctAnswer);
+
+
+
+
+
+
